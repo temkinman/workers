041a5e75e6e068d98a4fecfcb002192ee439ab5d
MS-CAS\admin <a.chegodar@softteco.com>
2023-02-12 00:14:38 +0300

    Added Entities

---
+++ Workers/Worker.Data.Access/Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Worker.Data.Access.Entities;
+
+public class Employee
+{
+    public Employee(string firstName, string lastName, string surName, DateTime bDay)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        SurName = surName;
+        BDay = bDay;
+    }
+
+    public Guid Id { get; init; }
+
+    [StringLength(50, ErrorMessage = "The FirstName value cannot be less 2 characters. ", MinimumLength = 2)]
+    public required string FirstName { get; init; }
+
+    [StringLength(50, ErrorMessage = "The FirstName value cannot be less 2 characters. ", MinimumLength = 2)]
+    public required string LastName { get; init; }
+    public string? SurName { get; init; }
+    public required DateTime BDay { get; init; }
+
+    List<Position> Positions { get; set; } = new List<Position>();
+}
---
+++ Workers/Worker.Data.Access/Entities/Position.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Worker.Data.Access.Entities;
+
+public class Position
+{
+    public Position(string name, int grade = 1)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Grade = grade;
+    }
+
+    public Guid Id { get; init; }
+    public required string Name { get; init; }
+
+    [Range(1, 15, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+    public int Grade { get; init; }
+    List<Employee> Employees { get; set; } = new List<Employee>();
+}
