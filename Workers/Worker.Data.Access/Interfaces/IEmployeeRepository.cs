de671c63ae2a71621bc5f49618226be16d1dbce6
MS-CAS\admin <a.chegodar@softteco.com>
2023-02-12 00:14:58 +0300

    added Interfaces

---
+++ Workers/Worker.Data.Access/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Worker.Data.Access.Entities;
+
+namespace Worker.Data.Access.Interfaces;
+
+public interface IEmployeeRepository
+{
+    Task<IEnumerable<Employee>> GetAllEmployeesAsync();
+    Task<Employee> GetEmployeeByIdAsync(Guid id);
+    Task<Employee> CreateEmployeeAsync(Employee employee);
+    Task<Employee> UpdateEmployeeAsync(Employee employee);
+    Task DeleteEmployeeAsync(Guid id);
+}
---
+++ Workers/Worker.Data.Access/Interfaces/IPositionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Worker.Data.Access.Entities;
+
+namespace Worker.Data.Access.Interfaces;
+public interface IPositionRepository
+{
+    Task<Employee> GetEmployeesByPositionAsync(Guid id);
+    Task<Position> CreatePositionAsync(Position position);
+    Task<Position> UpdatePositionAsync(Position position);
+    Task DeletePositionAsync(Guid id);
+}
