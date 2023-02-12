db35e9a89e48ba63dfdae1348618ae785440349d
Artyom Chegodar <>
2023-02-12 12:42:50 +0300

    changed models

--- Workers/Worker.Data.Access/Entities/Employee.cs
+++ Workers/Worker.Data.Access/Entities/Employee.cs
@@ -4,24 +4,15 @@ namespace Worker.Data.Access.Entities;
 
 public class Employee
 {
-    public Employee(string firstName, string lastName, string surName, DateTime bDay)
-    {
-        Id = Guid.NewGuid();
-        FirstName = firstName;
-        LastName = lastName;
-        SurName = surName;
-        BDay = bDay;
-    }
-
-    public Guid Id { get; init; }
+    public Guid Id { get; set; } = Guid.NewGuid();
 
     [StringLength(50, ErrorMessage = "The FirstName value cannot be less 2 characters. ", MinimumLength = 2)]
-    public required string FirstName { get; init; }
+    public required string FirstName { get; set; }
 
     [StringLength(50, ErrorMessage = "The FirstName value cannot be less 2 characters. ", MinimumLength = 2)]
-    public required string LastName { get; init; }
-    public string? SurName { get; init; }
-    public required DateTime BDay { get; init; }
+    public required string LastName { get; set; }
+    public string? SurName { get; set; }
+    public required DateTime BDay { get; set; }
 
-    List<Position> Positions { get; set; } = new List<Position>();
+    List<Position> Positions { get; set; } = new();
 }
--- Workers/Worker.Data.Access/Entities/Position.cs
+++ Workers/Worker.Data.Access/Entities/Position.cs
@@ -4,17 +4,10 @@ namespace Worker.Data.Access.Entities;
 
 public class Position
 {
-    public Position(string name, int grade = 1)
-    {
-        Id = Guid.NewGuid();
-        Name = name;
-        Grade = grade;
-    }
-
-    public Guid Id { get; init; }
-    public required string Name { get; init; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string Name { get; set; }
 
     [Range(1, 15, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
-    public int Grade { get; init; }
-    List<Employee> Employees { get; set; } = new List<Employee>();
+    public required int Grade { get; set; } = 1;
+    List<Employee> Employees { get; set; } = new();
 }
